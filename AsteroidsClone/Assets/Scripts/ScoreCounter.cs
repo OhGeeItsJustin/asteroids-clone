4a5cc299f224b6c9818631d69abacacf3233c1d2
Justin Blake Hill <justin.fallis@mohawkcollege.ca>
2023-12-07 19:52:58 -0500

    Display ScoreCounter To Gameboard

--- AsteroidsClone/Assets/Scripts/ScoreCounter.cs
+++ AsteroidsClone/Assets/Scripts/ScoreCounter.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreCounter : MonoBehaviour
 {
+    public TMP_Text scoreDisplay;
     private int score = 0;
-    public LifeCounter lifeCounter;
+    //public LifeCounter lifeCounter;
 
     public void AddToScore(int points)
     {
         score += points;
+        scoreDisplay.text = $"Score: {score} ";
+
     }
 
     // If player score is divisible by 5000 add a live
@@ -19,7 +23,7 @@ public class ScoreCounter : MonoBehaviour
         {
             if(score % 5000 == 0) 
             {
-                lifeCounter.AddToLives();
+                //lifeCounter.AddToLives();
             }
         }
     }
