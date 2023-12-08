4d20e6ed0c2ba44741bb0e5aa5c16aa3aefa5690
Justin Blake Hill <justin.fallis@mohawkcollege.ca>
2023-12-07 19:21:40 -0500

    Update LifeCount At Certain Scores In ScoreCounter.cs

--- AsteroidsClone/Assets/Scripts/ScoreCounter.cs
+++ AsteroidsClone/Assets/Scripts/ScoreCounter.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class ScoreCounter : MonoBehaviour
 {
     private int score = 0;
+    public LifeCounter lifeCounter;
 
     public void AddToScore(int points)
     {
         score += points;
+    }
+
+    // If player score is divisible by 5000 add a live
+    void Update()
+    {
+        if(score > 0)
+        {
+            if(score % 5000 == 0) 
+            {
+                lifeCounter.AddToLives();
+            }
+        }
     }
 }
