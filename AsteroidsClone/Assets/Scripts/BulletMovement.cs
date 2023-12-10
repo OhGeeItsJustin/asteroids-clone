df7cda7f4ba9b37322c38ef2f0b3e8e993f32fad
Justin Blake Hill <justin.fallis@mohawkcollege.ca>
2023-12-09 23:10:15 -0500

    Revert "Add Bullet Deletion After Certain In BulletMovement.cs"
    This reverts commit dbaa381c6ab32566e394247e845e184be9dabb36.

--- AsteroidsClone/Assets/Scripts/BulletMovement.cs
+++ AsteroidsClone/Assets/Scripts/BulletMovement.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class BulletMovement : MonoBehaviour
 {
-    public float maxDistance;
-    public float currentDistance;
     public float speed;
 
     void Start()
     {
-        maxDistance = 7;
-        currentDistance = 0;
         speed = 10f;
     }
 
@@ -21,12 +17,5 @@ public class BulletMovement : MonoBehaviour
         Vector3 moveBullet = new Vector3();
         moveBullet += transform.up * speed;
         transform.position += moveBullet * Time.deltaTime;
-
-        // Track distance bullet has moved and delete bullet object after hitting max distance
-        currentDistance += speed * Time.deltaTime;
-        if (currentDistance > maxDistance)
-        {
-            Destroy(gameObject);
-        }
     }
 }
