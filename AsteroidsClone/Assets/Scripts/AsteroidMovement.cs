9d6ca4b5967cbdf4a1015196040b6a9021418110
Justin Blake Hill <justin.fallis@mohawkcollege.ca>
2023-12-09 21:28:24 -0500

    Create Split Function In LargeAsteroid.cs

--- AsteroidsClone/Assets/Scripts/AsteroidMovement.cs
+++ AsteroidsClone/Assets/Scripts/AsteroidMovement.cs
@@ -8,7 +8,7 @@ public class AsteroidMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = 5f;
+        speed = 2f;
     }
 
     // Update is called once per frame
--- AsteroidsClone/Assets/Scripts/LargeAsteroid.cs
+++ AsteroidsClone/Assets/Scripts/LargeAsteroid.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LargeAsteroid : MonoBehaviour
 {
-
+    public GameObject mediumAsteroid;
     void Start()
     {
         float x = Random.Range(-9f, 9f);
@@ -20,6 +20,20 @@ public class LargeAsteroid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void SplitLargeAsteroid()
+    {
+        // Give bullet the players position
+        Vector3 position = transform.position;
+        GameObject mediumAsteroidOne = Instantiate(mediumAsteroid, position, Quaternion.identity);
+        GameObject mediumAsteroidTwo = Instantiate(mediumAsteroid, position, Quaternion.identity);
+
+        // Display to screen
+        mediumAsteroidOne.SetActive(true);
+        mediumAsteroidTwo.SetActive(true);
+
+        Destroy(gameObject);
     }
 }
