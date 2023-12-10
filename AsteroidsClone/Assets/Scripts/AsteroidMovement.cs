68f4c914232a41a9e577dec85ba675f2b021811a
Justin Blake Hill <justin.fallis@mohawkcollege.ca>
2023-12-09 21:10:18 -0500

    Create Upward Movement In AsteroidMovement.cs

--- AsteroidsClone/Assets/Scripts/AsteroidMovement.cs
+++ AsteroidsClone/Assets/Scripts/AsteroidMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class AsteroidMovement : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 moveBullet = new Vector3();
+        moveBullet += transform.up * speed;
+        transform.position += moveBullet * Time.deltaTime;
     }
 }
