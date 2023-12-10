01cb0348f2b6a6e65ad17e412a2ef68630c54545
Justin Blake Hill <justin.fallis@mohawkcollege.ca>
2023-12-09 21:33:27 -0500

    Add Random Rotation To MediumAsteroid.cs

--- AsteroidsClone/Assets/Scripts/MediumAsteroid.cs
+++ AsteroidsClone/Assets/Scripts/MediumAsteroid.cs
@@ -7,7 +7,11 @@ public class MediumAsteroid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float randomRotation = Random.Range(0, 360);
+
+        // Give bullet the players rotation
+        float currentZ = transform.rotation.eulerAngles.z;
+        transform.rotation = Quaternion.Euler(0, 0, currentZ + randomRotation);
     }
 
     // Update is called once per frame
