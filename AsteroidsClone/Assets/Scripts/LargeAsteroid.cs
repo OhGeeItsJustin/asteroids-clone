299124f06832e058542a02cbad543005a0487670
Justin Blake Hill <justin.fallis@mohawkcollege.ca>
2023-12-09 21:11:01 -0500

    Create Random Rotation In LargeAsteroid.cs

--- AsteroidsClone/Assets/Scripts/LargeAsteroid.cs
+++ AsteroidsClone/Assets/Scripts/LargeAsteroid.cs
@@ -10,6 +10,11 @@ public class LargeAsteroid : MonoBehaviour
         float x = Random.Range(-9f, 9f);
         float y = Random.Range(2f, 5f);
         transform.position = new Vector3(x, y, 0);
+        float randomRotation = Random.Range(0, 360);
+
+        // Give bullet the players rotation
+        float currentZ = transform.rotation.eulerAngles.z;
+        transform.rotation = Quaternion.Euler(0, 0, currentZ + randomRotation);
     }
 
     // Update is called once per frame
